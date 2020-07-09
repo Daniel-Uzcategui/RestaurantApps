3a04e90d9d6ec17734a12b6e4737ca058fb5cfc6
Daniel Uzcategui <44720483+Daniel-Uzcategui@users.noreply.github.com>
2020-07-08 22:17:23 -0400

    Menu changes

--- App/RestaurantAdmin/src/pages/Menu/menu.vue
+++ App/RestaurantAdmin/src/pages/Menu/menu.vue
@@ -11,6 +11,15 @@
       :selected.sync="selected"
     >
     <template v-slot:top-right>
+      <q-label class="q-pa-md">STOCK POR SEDE</q-label>
+      <q-select
+        bg-color="white"
+        outlined
+        v-model="sede"
+        :options="locList"
+        style="width: 250px"
+        behavior="menu"
+      />
         <q-btn-group flat push >
           <q-btn flat color="white" push label="Agregar" icon="fas fa-plus" @click="addrow"/>
           <q-btn flat color="white" push label="Eliminar" icon="fas fa-minus" @click="delrow"/>
@@ -96,6 +105,19 @@
             </q-popup-edit>
           </q-td>
 
+          <q-td key="stock" :props="props">
+            <div class="text-pre-wrap">{{ props.row.stock }}</div>
+            <q-popup-edit :value="props.row.stock">
+              <q-input
+                @input="(e) => saved(e, props.row.stock, props.row.id, 'stock')"
+                :value="props.row.stock"
+                dense
+                autofocus
+                type="number"
+              />
+            </q-popup-edit>
+          </q-td>
+
           <q-td key="price" :props="props">
             <div class="text-pre-wrap">{{ props.row.price }}</div>
             <q-popup-edit :value="props.row.price">
@@ -144,11 +166,12 @@
 <script>
 const columns = [
   { name: 'desc', style: 'min-width: 80px; width: 100px', align: 'left', label: 'Nombre', field: 'name' },
-  { name: 'descripcion', style: 'min-width: 80px; width: 120px', align: 'left', label: 'Descripción', field: 'descripcion' },
-  { name: 'categoria', align: 'center', label: 'Categoria', field: 'categoria' },
+  { name: 'descripcion', style: 'min-width: 80px; width: 100px', align: 'left', label: 'Descripción', field: 'descripcion' },
+  { name: 'categoria', align: 'left', label: 'Categoria', field: 'categoria' },
   { name: 'estatus', align: 'center', label: 'Activar', field: 'estatus' },
   { name: 'photo', align: 'center', label: 'Foto', field: 'photo' },
   { name: 'FechaAct', label: 'Fecha Activación', field: 'FechaAct' },
+  { name: 'stock', align: 'center', label: 'Stock', field: 'stock' },
   { name: 'price', align: 'center', label: 'Precio', field: 'price' },
   { name: 'extras', align: 'center', label: 'Extras', field: 'extras' }
 ]
@@ -162,6 +185,15 @@ export default {
   computed: {
     ...mapGetters('menu', ['categorias', 'menu', 'listcategorias', 'plaincategorias', 'listextras', 'plainExtras']),
     ...mapGetters('user', ['currentUser']),
+    ...mapGetters('localization', ['localizations']),
+    locList () {
+      return this.localizations.map(x => {
+        return {
+          value: x.id,
+          label: x.name
+        }
+      })
+    },
     meta () {
       return {
         id: this.currentUser.id,
@@ -176,6 +208,7 @@ export default {
   },
   data () {
     return {
+      sede: null,
       columns,
       selected: [],
       popupEditData: '',
@@ -187,6 +220,7 @@ export default {
     this.bindMenu()
     this.bindExtras()
     this.bindCategorias()
+    this.bindLocalizations()
   },
   methods: {
     resetPhotoType () {
@@ -203,6 +237,7 @@ export default {
       console.log(`retain original value = ${initialValue}, canceled value = ${val}`)
     },
     ...mapActions('menu', ['setValue', 'addRow', 'delrows', 'bindMenu', 'bindExtras', 'bindCategorias']),
+    ...mapActions('localization', ['bindLocalizations']),
     delrow () {
       this.delrows({ payload: this.selected, collection: 'menu' })
     },
