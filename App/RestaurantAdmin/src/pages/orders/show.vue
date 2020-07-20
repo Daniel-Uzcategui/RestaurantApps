e0a598e346cfd241291b73ac8fac5bbff7e772fd
Pedro Porras <pporras@gmail.com>
2020-07-20 10:37:10 -0430

    pequeño fix a soporte de pago

--- App/RestaurantAdmin/src/pages/orders/show.vue
+++ App/RestaurantAdmin/src/pages/orders/show.vue
@@ -35,10 +35,6 @@
           <q-select :value="order.typePayment" @input="(e) => saved(e, this.$route.query.Order_Id, 'typePayment')" standout="bg-teal text-white"
             :options="typePayment_options" label="Tipo de Pago" />
         </div>
-        <div class="header-cell col-3" v-if="order.typePayment!=='punto'">
-          <label>Soporte de pago</label><br>
-          <i class="fa fa-search" @click="photoDiag=true"></i>
-        </div>
          <div class="flex-break q-pa-md"></div>
          <div class="header-cell col-6">
             <q-input label="punto de Referencia" :value="puntoRef"  filled type="textarea" placeholder="Punto de referencia"  disabled/>
@@ -46,6 +42,13 @@
          <div class="header-cell col-6">
             <q-input label="Dirección de entrega" :value="addressDelivery"  filled type="textarea" placeholder="Dirección del cliente" disabled />
          </div>
+      </div>
+      <div class="column items-center filled">
+        <div class="col">
+      <label><strong>Soporte de pago</strong></label>
+      <viewer :img="order.photo"></viewer>
+      <span>Presione en la imagen para ver</span>
+      </div>
       </div>
      </div>
       <div class="flex-break q-py-md "></div>
@@ -115,19 +118,6 @@
           Los siguientes campos son requeridos
           {{messageError}}
         </q-card-section>
-      </q-card>
-    </q-dialog>
-    <q-dialog v-model="photoDiag">
-      <q-card>
-        <q-card-section class="row items-center q-pb-none">
-          <div class="text-h6">Soporte de pago</div>
-          <q-space />
-          <q-btn icon="close" flat round dense v-close-popup />
-        </q-card-section>
-       <q-card-section>
-          <viewer :img="order.photo">
-          </viewer>
-       </q-card-section>
       </q-card>
     </q-dialog>
 </q-page>
