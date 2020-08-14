a076144bee3fbc807b5390df37c8cdaea60ca0e2
Pedro Porras <pporras@gmail.com>
2020-08-14 18:37:09 -0430

    fix incorporacion de telefono

--- App/RestaurantClient/src/store/auth/actions.js
+++ App/RestaurantClient/src/store/auth/actions.js
@@ -3,18 +3,18 @@ import User from '../../models/User.js'
 
 export const addUserToUsersCollection = async (state, userRef) => {
   const
-    { email, nombre, apellido, cedula, id } = state,
-    user = new User({ email, nombre, apellido, cedula, id })
+    { email, nombre, apellido, cedula, id, phone } = state,
+    user = new User({ email, nombre, apellido, cedula, id, phone })
   return userRef.set(user)
 }
 
 export const createNewUser = async function ({ dispatch, commit }, data) {
   const $fb = this.$fb
-  const { email, password, nombre, apellido, cedula } = data
+  const { email, password, nombre, apellido, cedula, phone } = data
   const fbAuthResponse = await $fb.createUserWithEmail(email, password)
   const id = fbAuthResponse.user.uid
   const userRef = $fb.userRef('users', id)
-  return addUserToUsersCollection({ email, nombre, apellido, cedula, id }, userRef)
+  return addUserToUsersCollection({ email, nombre, apellido, cedula, id, phone }, userRef)
 }
 
 export const loginUser = async function ({ commit }, payload) {
