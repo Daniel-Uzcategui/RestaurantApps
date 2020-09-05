5867df42901527c07c4c21345a57c654c221bbf8
Pedro Porras <pporras@gmail.com>
2020-09-05 15:59:41 -0430

    fin enviroments

--- App/RestaurantAdmin/src/boot/serverConnection.js
+++ App/RestaurantAdmin/src/boot/serverConnection.js
@@ -2,7 +2,7 @@ import firebaseService from '../services/firebase'
 
 export default ({ router, store, Vue }) => {
   console.log({ process: process.env.MODE })
-  if (process.env.environments.ENV_DEV !== 'pwa') {
+  if (process.env.environments.ENV_DEV === 'pwa') {
     fetch('/__/firebase/init.json').then(async response => {
       firebaseService.fBInit(await response.json())
       firebaseService.auth().onAuthStateChanged((currentUser) => {
