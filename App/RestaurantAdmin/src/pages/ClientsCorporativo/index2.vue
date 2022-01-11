f0db9da9383c2cc47ddd141f189eb9c2a44370ab
pataxjose <joseg.cva@gmail.com>
2022-01-11 14:01:17 -0400

    crud de suculsales

--- App/RestaurantAdmin/src/pages/ClientsCorporativo/index2.vue
+++ App/RestaurantAdmin/src/pages/ClientsCorporativo/index2.vue
@@ -6,26 +6,50 @@
     <div class="row justify-center items-center">
     <q-card class="q-pa-s q-cardGlass" style="border-radius: 30px;">
        <q-card-section>
-         <q-input label="Nombre Sucursal"> </q-input>
-         <q-input label="Razón Social"> </q-input>
-         <h7><q-select label="----" id="sortBy"  v-model="model2" :options="options2">
+         <q-input label="Nombre Sucursal" v-model="nombresurculsal"> </q-input>
+         <q-input label="Razón Social" v-model="razon"> </q-input>
+          <q-select label="Nacionalidad"  id="sortBy3" v-model="prefijo" :options="options2" >
 
-            </q-select> <q-input label="Rif"> </q-input> </h7>
+            </q-select>
+             <q-input label="Rif" v-model="numerorif"> </q-input>
 
              <q-select label="Tipo Pago"  id="sortBy1" v-model="tipopago" :options="options" >
              </q-select>
-              <q-select label="Vendedores" id="sortBy2"  v-model="selle" :options="Vendedores" >
+              <q-select label="Vendedores" id="sortBy2"  v-model="selle" :options="Vendedores" @input="obtenervendedor()">
               </q-select>
-              <q-input label="Dias de Credito"> </q-input>
+              <q-input label="Dias de Credito" v-model="diacredito"> </q-input>
       </q-card-section>
        <div class="center">
-          <q-btn color="primary" text-color="white" label="Guardar" />
+          <q-btn color="primary" text-color="white" label="Guardar" @click="guardar(nombresurculsal,razon,prefijo,numerorif,tipopago,selle,idselle,diacredito)"/>
           &nbsp;&nbsp;
           <q-btn color="primary" text-color="white" label="Cancelar" />
            </div>
     </q-card>
 
     </div>
+     <q-dialog v-model="clieEditar" transition-show="rotate" transition-hide="rotate">
+      <q-card>
+        <q-card-section>
+           <q-input label="Nombre Sucursal" v-model="nombresurculsal"> </q-input>
+         <q-input label="Razón Social" v-model="razon"> </q-input>
+          <q-select label="Nacionalidad"  id="sortBy3" v-model="prefijo" :options="options2" >
+
+            </q-select>
+             <q-input label="Rif" v-model="numerorif"> </q-input>
+
+             <q-select label="Tipo Pago"  id="sortBy1" v-model="tipopago" :options="options" >
+             </q-select>
+              <q-select label="Vendedores" id="sortBy2"  v-model="selle" :options="Vendedores" @input="obtenervendedor()">
+              </q-select>
+              <q-input label="Dias de Credito" v-model="diacredito"> </q-input>
+        </q-card-section>
+
+        <q-card-actions align="right">
+            <q-btn flat label="Accept" color="primary"  @click="guardarEditado(idsuculsal,nombresurculsal,razon,prefijo,numerorif,tipopago,idselle,selle,diacredito)" v-close-popup/>
+            <q-btn flat label="Cancelar" color="primary" v-close-popup/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
     <div class="row justify-center items-center">>
     <q-table  class="column  justify-center items-center " title="Sucursales"
       :data="corporativo"
@@ -36,12 +60,12 @@
       >
        <template v-slot:body-cell-boton1 ="props" id=1>
          <q-td :props="props" class="q-pa-md q-gutter-sm">
-            <q-btn  color="primary" label="Editar"  />
+            <q-btn  color="primary" label="Editar" @click="editar(props.row)" />
            </q-td>
        </template>
         <template v-slot:body-cell-boton2 ="props" id=2>
          <q-td :props="props" class="q-pa-md q-gutter-sm">
-            <q-btn  color="primary" label="Quitar"   />
+            <q-btn  color="primary" label="Quitar" @click="borrar(props.row)"  />
            </q-td>
        </template>
      </q-table >
@@ -53,26 +77,34 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data () {
     return {
+      nombresurculsal: '',
+      clieEditar: false,
+      numerorif: '',
+      idsuculsal: '',
+      razon: '',
       selle: '',
+      idselle: '',
       tipopago: '',
+      diacredito: 0,
       model2: '',
+      prefijo: '',
       options: ['Contado', 'Credito'],
       options2: ['J', 'V', 'E', 'G'],
       Vendedores: [],
       columns: [
-        { name: 'Nombre', required: false, label: 'Nombre', align: 'left', field: row => row.name, sortable: true },
-        { name: 'Rif', required: false, label: 'Rif', field: row => row.Rif.prefijo + '-' + row.Rif.numero, align: 'left', sortable: true },
-        { name: 'Razon', required: false, label: 'Razon Social', field: row => row.RazonSocial, align: 'left', sortable: true },
-        { name: 'Vendedor', required: false, label: 'Vendedor', field: row => row.Vendedor.name, align: 'left', sortable: true },
-        { name: 'creditDays', required: false, label: 'Razon Social', field: row => row.RazonSocial, align: 'left', sortable: true },
-        { name: 'tipoPago', required: false, label: 'Tipo Pago', field: row => row.tipoPago, align: 'left', sortable: true },
+        { name: 'Nombre', required: false, label: 'Nombre', align: 'left', field: row => row?.name, sortable: true },
+        { name: 'Rif', required: false, label: 'Rif', field: row => row.Rif?.prefijo + '-' + row.Rif?.numero, align: 'left', sortable: true },
+        { name: 'Razon', required: false, label: 'Razon Social', field: row => row?.RazonSocial, align: 'left', sortable: true },
+        { name: 'Vendedor', required: false, label: 'Vendedor', field: row => row.Vendedor?.name, align: 'left', sortable: true },
+        { name: 'creditDays', required: false, label: 'Dias Credito', field: row => row?.creditDays, align: 'left', sortable: true },
+        { name: 'tipoPago', required: false, label: 'Tipo Pago', field: row => row?.tipoPago, align: 'left', sortable: true },
         { name: 'boton1', required: false, label: '', align: 'left', sortable: true },
         { name: 'boton2', required: false, label: '', align: 'left', sortable: true }
       ]
     }
   },
   methods: {
-    ...mapActions('corporativos', ['bindcorporativo']),
+    ...mapActions('corporativos', ['bindcorporativo', 'setValuenew', 'setValueborrar', 'setValueEditados']),
     ...mapActions('client', ['bindOnlyVendedor']),
     mostrar () {
       let obj
@@ -82,6 +114,85 @@ export default {
         obj = this.vendedor[i]
         this.Vendedores.push(obj.nombre)
       }
+    },
+    guardar (nombre, razon, prefijo, numerorif, tipopago, selle, idselle, diacredito) {
+      console.log('los valores del prefijo', prefijo)
+      this.setValuenew({
+        id: this.idClientSel,
+        name: nombre,
+        RazonSocial: razon,
+        Rif: {
+          prefijo: prefijo,
+          numero: numerorif
+        },
+        Vendedor: {
+          id: idselle,
+          name: selle
+        },
+        creditDays: diacredito,
+        tipoPago: tipopago
+
+      })
+      this.inicializar()
+    },
+    inicializar () {
+      this.nombresurculsal = ''
+      this.razon = ''
+      this.prefijo = ''
+      this.numerorif = ''
+      this.tipopago = ''
+      this.selle = ''
+      this.diacredito = 0
+    },
+    borrar (objeto) {
+      console.log('estos son los valores', objeto)
+      this.setValueborrar({ idcliente: this.idClientSel,
+        id: objeto.id })
+    },
+    editar (objeto) {
+      console.log('los valores', objeto)
+      this.idsuculsal = objeto.id
+      this.nombresurculsal = objeto?.name
+      this.razon = objeto?.RazonSocial
+      this.prefijo = objeto.Rif?.prefijo
+      this.numerorif = objeto.Rif?.numero
+      this.tipopago = objeto?.tipoPago
+      this.selle = objeto.Vendedor?.name
+      this.diacredito = objeto?.creditDays
+      this.idselle = objeto.Vendedor?.id
+      this.clieEditar = true
+    },
+    guardarEditado (idsuculsal, nombresurculsal, razon, prefijo, numerorif, tipopago, idselle, selle, diacredito) {
+      this.setValueEditados({
+        idcliente: this.idClientSel,
+        id: idsuculsal,
+        datos: {
+
+          name: nombresurculsal,
+          RazonSocial: razon,
+          Rif: {
+            prefijo: prefijo,
+            numero: numerorif
+          },
+          Vendedor: {
+            id: idselle,
+            name: selle
+          },
+          creditDays: diacredito,
+          tipoPago: tipopago
+        }
+      })
+      this.inicializar()
+    },
+    obtenerprefijo () {
+      this.prefijo = this.model2
+    },
+    obtenervendedor () {
+      let obj
+      obj = this.vendedor.find(x => x.nombre === this.selle)
+      console.log('este es el vendedor', obj)
+      this.idselle = obj?.id
+      return this.idselle
     }
   },
   computed: {
--- App/RestaurantAdmin/src/store/corporativo/actions.js
+++ App/RestaurantAdmin/src/store/corporativo/actions.js
@@ -4,5 +4,49 @@ import { firestore } from '../../services/firebase/db.js'
 
 export const bindcorporativo = firestoreAction(({ bindFirestoreRef }, payload) => {
   console.log('bindClients')
-  return bindFirestoreRef('corporativo', firestore().collection('clients').doc(payload.id).collection('branches'))
+  return bindFirestoreRef('corporativo', firestore().collection('clients').doc(payload.id).collection('branches').where('softDelete', '==', 0))
 })
+export const setValuenew = async function (state, payload) {
+  let result
+  console.log('valores a agregar', payload)
+  result = await firestore().collection('clients').doc(payload.id).collection('branches')
+    .add({
+      name: payload.name,
+      RazonSocial: payload.RazonSocial,
+      Rif: payload.Rif,
+      Vendedor: payload.Vendedor,
+      creditDays: payload.creditDays,
+      tipoPago: payload.tipoPago,
+      softDelete: 0
+    })
+  return result
+}
+export const setValueborrar = async function (state, payload) {
+  let result
+  console.log('el id para borrar', payload.id)
+  result = await firestore().collection('clients')
+    .doc(payload.idcliente).collection('branches').doc(payload.id)
+    .set({
+      softDelete: 1
+    }, { merge: true })
+  return result
+}
+export const setValueEditados = function (state, payload) {
+  console.log('registro', payload)
+
+  firestore().collection('clients')
+    .doc(payload.idcliente).collection('branches').doc(payload.id)
+    .set({
+      name: payload.datos.name,
+      RazonSocial: payload.datos.RazonSocial,
+      Rif: payload.datos.Rif,
+      Vendedor: payload.datos.Vendedor,
+      creditDays: payload.datos.creditDays,
+      tipoPago: payload.datos.tipoPago,
+      softDelete: 0
+    }, { merge: true }).then(() => {
+      console.log('se actualizo')
+    }).catch((e) => {
+      console.log('error update de banches!')
+    })
+}
