3c808330d1cacf1f4d94257526df20dc0958712b
pataxjose <joseg.cva@gmail.com>
2022-05-02 14:33:04 -0400

    responsivo de encomienda

--- App/RestaurantAdmin/src/pages/jetes/encomienda.vue
+++ App/RestaurantAdmin/src/pages/jetes/encomienda.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
-        <h6>Encomienda</h6>
+  <q-page  class= 'q-mt-lg'>
+<div class="q-gutter-md" style="margin-left:14px;margin-right:16px">
+      <q-card class="q-cardGlass">
+        <q-card-section class="q-cardtop header">
+          <div class="text-h5" style="padding-bottom: 50px;">Encomienda</div>
+          <div class="absolute-bottom-right q-pa-md">
+            <q-btn
+              flat
+              color="white"
+              icon="arrow_back"
+              @click="$router.replace('/encomienda/index')"
+            >
+            </q-btn>
+          </div>
+        </q-card-section>
+        <div class="filled"></div>
+        <q-card-section class="q-mr-lg">
 <courier
 :propsEcomienda ="Encomienda"
 />
-    </div>
+</q-card-section>
+</q-card>
+</div>
+</q-page>
 </template>
 <script>
 import { mapGetters } from 'vuex'
