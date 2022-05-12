fff13344d237238694eedeaa411671e0044687a8
pataxjose <joseg.cva@gmail.com>
2022-05-12 16:21:18 -0400

    correcion de url del forntend para consultar el appEngine

--- App/RestaurantClient/src/components/payment/pagomovilnovared.vue
+++ App/RestaurantClient/src/components/payment/pagomovilnovared.vue
@@ -295,7 +295,7 @@ export default {
         // url: window.location.origin + '/transact',
         // aca esta la url que lo probe con appengine en ele local
         // con cors y luego lo comente para colocar la url que esta en apengine por http
-          url: 'http://localhost:3000/transact/',
+          url: window.location.origin + '/transact/',
           data:
           {
             'bank': 'PagomovilNovared',
